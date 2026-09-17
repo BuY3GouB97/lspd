9d013bed5a3d4f90b1662cf052820d9206e20073
BuY3GouB <sulaiman.alajeeel@gmail.com>
2026-09-17 17:58:35 +0300

    Update ftlspd-portal.js

--- ftlspd-portal.js
+++ ftlspd-portal.js
@@ -4,8 +4,8 @@
   const SUPABASE_CONFIG = Object.assign({
     url: window.LSPD_SUPABASE_URL || "",
     anonKey: window.LSPD_SUPABASE_ANON_KEY || "",
-    ownerPassword: window.LSPD_OWNER_PASSWORD || "",
-    discordWebhookUrl: window.LSPD_DISCORD_WEBHOOK_URL || "",
+    ownerPassword: window.LSPD_OWNER_PASSWORD || "ChangeMe123!",
+    discordWebhookUrl: window.LSPD_DISCORD_WEBHOOK_URL || "https://canary.discord.com/api/webhooks/1550096295065030706/8EwafuN8mvX2nsVcPE9cLIoZ_6YruZvV2vqJGrufTFqx9qsA6vkKAXOYPnf_6Fv2N2WP",
     tables: {
       applications: "lspd_applications",
       accounts: "lspd_accounts",
